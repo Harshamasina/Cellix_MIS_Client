5f79542e35f9575d1e9e47e00b948a1a84eb0673
Harshamasina <18uec021@lnmiit.ac.in>
2023-02-09 16:23:22 +0530

    changed

--- src/Components/Body/Home/Home.js
+++ src/Components/Body/Home/Home.js
@@ -1,8 +1,23 @@
+import { Parallax } from 'react-parallax';
+
 const Home = () => {
+    const img = "https://cellix-bio-mis.s3.ap-south-1.amazonaws.com/web+assets/Brain.jpg";
     return(
         <div>
-            <h1>Home</h1>
+            <div>
+                <Parallax bgImage={ img } strength={350} bgImageAlt="parallaximg">
+                    <div className='ParallaxContainer'>
+                        <div className="ParallaxDiv">
+                            <div className='HomePageContent'>
+                                <h1>Cellix Bio Management Information Systems</h1>
+                                <h2>For it's Intellectual Property</h2>
+                            </div>
+                        </div>
+                    </div>
+                </Parallax>
+            </div>
         </div>
     );
 }
+
 export default Home;
